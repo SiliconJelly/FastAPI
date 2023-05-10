f38c77faa68ddfc13fda22f3a82de52506f4fbe0
Tanjim Mohamed Nirjhor <65353587+SiliconJelly@users.noreply.github.com>
2023-05-10 14:54:45 +0600

    second commit
    Handling GET request

--- coding-with-imran/main.py
+++ coding-with-imran/main.py
@@ -1,25 +1,46 @@
-# a simple FastAPI web application with a single
-# route that handles GET requests to the root URL
-
 from typing import Union  # class
 from fastapi import FastAPI  # class
+import random as rd
 
 app = FastAPI()  # creates a new object
 
 
-@app.get("/")  # GET request is made to the root URL ("/")
+items = {
+    "1": {"name": "Foo", "price": 50.2},
+    "2": {"name": "Bar", "price": 62},
+    "3": {"name": "Baz", "price": 50.8},
+    "4": {"name": "Too", "price": 54.3},
+    "5": {"name": "Boo", "price": 20.6},
+}
+
+# endpoint in postman: http://localhost:5000/item
+@app.get("/item")
+async def read_item():
+    random_key = rd.choice(list(items.keys())) ## randomly selecting a key in items
+    random_value = items[random_key]  ## calling the value of the selected key
+    return {"message": random_value}  ## returning the value
+
+# endpoint in postman: http://localhost:5000/items 
+@app.get("/items")
+async def read_items():
+    return items     ## returning all the value
+
+# start the app (in case, path known) → uvicorn main:app --port 5000 --reload --log-level debug
+# my terminal couldn't find the path, so I had to tell python with python -m (rest of the script) →
+# python -m uvicorn main:app --port 5000 --reload --log-level debug
+
+
+@app.get("/")
 def read_root():
     return {"Hello from FastAPI"}
 
 
-# after running uvicorn, we should be getting:
 '''
-INFO: Uvicorn running on http://127.0.0.1:8000 (Press CTRL+C to quit)
-INFO: Started reloader process [12345]
-INFO: Started server process [12346]
-INFO: Waiting for application startup.
-INFO: Application startup complete.
+Expected Output:
+INFO:     Will watch for changes in these directories: ['C:\\Users\\SiliconJelly\\Desktop\\Projects\\coding-with-imran']
+INFO:     Uvicorn running on http://127.0.0.1:5000 (Press CTRL+C to quit)
+INFO:     Started reloader process [3764] using WatchFiles
+INFO:     Started server process [16740]
+INFO:     Waiting for application startup.
+INFO:     Application startup complete.
 '''
-
-# and here is the auto-generated documentation:
-# http://127.0.0.1:8000/docs#/default/read_root__get
